b4e23bbb4001a21a29391075f5a10611613ae438
Themba <SHNMBA004@uct.ac.za>
2020-02-29 01:25:36 +0200

    Added the volimage h class

---
+++ VolImage.h
@@ -0,0 +1,15 @@
+//Mbaliyethemba Shangase
+//2 March 2019
+//VolImage class for Assignment 2
+
+#ifndef _VOLIMAGE_H
+#define _VOLIMAGE_H
+
+#include <vector>
+#include <string>
+#include <cstring>
+#include <fstream>
+
+namespace SHNMBA004{
+	private
+}
